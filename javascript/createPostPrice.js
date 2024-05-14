f9725f5b3d3a3b41cf102f1c39577df4bb83f1e3
NghiaPham2805 <s4028630@rmit.edu.vn>
2024-05-14 15:14:21 +0700

    I did it

--- javascript/createPostPrice.js
+++ javascript/createPostPrice.js
@@ -4,9 +4,13 @@ form.addEventListener('submit', (e) => {
     e.stopPropagation();
     const title = document.querySelector('#title');
     const price = document.querySelector('#price'); 
+    const sortby = document.querySelector('#sortby');
+    const fileInput = document.querySelector('.drop-zone__input');
     
     title.classList.remove('error-input');
     price.classList.remove('error-input'); 
+    sortby.classList.remove('error-input');
+    fileInput.classList.remove('error-input');
 
     let error=false;
     let errorList='';
@@ -16,13 +20,28 @@ form.addEventListener('submit', (e) => {
         title.focus();
         errorList += '<li>Title is required</li>';
     }
-    
+
+    if (sortby.value !== 'dog' && sortby.value !== 'cat') {
+        error = true;
+        sortby.classList.add('error-input');
+        sortby.focus();
+        errorList += '<li>Please select a pet type</li>';
+    }
+
     if (price.value === '' || price.value === null) {
         error = true;
         price.classList.add('error-input');
         price.focus();
         errorList += '<li>Price is required</li>';
     }
+
+    if (!fileInput.files.length) {
+        error = true;
+        fileInput.classList.add('error-input');
+        fileInput.focus();
+        errorList += '<li>Please upload an image</li>';
+    }
+
     let errors="<ul class='error-list'>"+errorList+"</ul>";
     if(error){
         Swal.fire({
